59797eff48f64d0bedaa58ef67d37e1b64c9900a
saxsir <saxsir.256+github@gmail.com>
2017-04-13 20:10:39 +0900

    build用にディレクトリ切らなくていいかと思ったのでbuildターゲット修正

--- server/server.go
+++ server/server.go
@@ -1,13 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	fmt.Println("Hello, golang world!!")
 }
