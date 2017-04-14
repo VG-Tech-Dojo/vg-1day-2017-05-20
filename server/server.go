b430ca465415327a0e60efda98854eb3347fd17e
saxsir <saxsir.256+github@gmail.com>
2017-04-14 18:23:58 +0900

    とりあえずginでサンプルつくろう、ということにしたので他は消した

--- server/server.go
+++ server/server.go
@@ -1,25 +1,13 @@
 package main
 
-import (
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-
-	"github.com/saxsir/vg-1day-2017/server/controller"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// Echo instance
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Route => handler
-
-	// Start server
-	message := &controller.Message{}
-	e.GET("/", message.Root)
-
-	e.Logger.Fatal(e.Start(":1323"))
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
