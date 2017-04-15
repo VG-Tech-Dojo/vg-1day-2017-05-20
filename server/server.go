c6ae73bb51b61e7c637bb4023d57564c4d2ea492
saxsir <saxsir.256+github@gmail.com>
2017-04-15 19:45:13 +0900

    sqlite3プラグイン入れてサーバー起動するところまで

--- server/server.go
+++ server/server.go
@@ -1,13 +1,55 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"flag"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Server is whole server implementation for this app
+type Server struct {
+	db     *sql.DB
+	Engine *gin.Engine
+}
+
+func NewServer() *Server {
+	return &Server{
+		Engine: gin.Default(),
+	}
+}
+
+func (s *Server) Init(dbconf, env string) error {
+	db, err := sql.Open("sqlite3", "./sample.db")
+	if err != nil {
+		return err
+	}
+	s.db = db
+	return nil
+}
+
+func (s *Server) Close() error {
+	return s.db.Close()
+}
+
+func (s *Server) Run() {
+	s.Engine.Run()
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	var (
+		dbconf = flag.String("dbconf", "dbconfig.yml", "database configuration file.")
+		env    = flag.String("env", "development", "application envirionment (production, development etc.)")
+	)
+	flag.Parse()
+
+	s := NewServer()
+	if err := s.Init(*dbconf, *env); err != nil {
+		log.Fatalf("fail to start server: ", err)
+	}
+	defer s.Close()
+
+	s.Run()
 }
