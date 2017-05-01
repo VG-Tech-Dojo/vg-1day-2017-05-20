5b72479efea6750cc39d97828535d78f1f545544
saxsir <saxsir.256+github@gmail.com>
2017-05-01 11:17:06 +0900

    anonymous => ""
    ```
    ~/src/github.com/VG-Tech-Dojo/vg-1day-2017/original on  modify-schema
    ⌚ 11:19:00
    $ sqlite3 dev.db
    s-sasamoto-m.voyagegroup.local
    SQLite version 3.8.10.2 2015-05-20 18:17:19
    Enter ".help" for usage hints.
    sqlite> .schema message
    CREATE TABLE message (
        id INTEGER NOT NULL PRIMARY KEY,
        body TEXT NOT NULL DEFAULT "",
        sender_name TEXT NOT NULL DEFAULT "",
        created TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime')),
        updated TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime'))
    );
    sqlite> select * from message;
    1|hoge||2017-05-01 11:16:47|2017-05-01 11:16:47
    2|fuga||2017-05-01 11:16:50|2017-05-01 11:16:50
    3|piyo||2017-05-01 11:16:53|2017-05-01 11:16:53
    sqlite> ^D
    
    ~/src/github.com/VG-Tech-Dojo/vg-1day-2017/original on  modify-schema
    ⌚ 11:19:10
    $ sqlite3 _etc/seed.db
    s-sasamoto-m.voyagegroup.local
    SQLite version 3.8.10.2 2015-05-20 18:17:19
    Enter ".help" for usage hints.
    sqlite> .schema message
    CREATE TABLE message (
        id INTEGER NOT NULL PRIMARY KEY,
        body TEXT NOT NULL DEFAULT "",
        sender_name TEXT NOT NULL DEFAULT "",
        created TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime')),
        updated TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime'))
    );
    sqlite> select * from message;
    1|hoge||2017-05-01 11:16:47|2017-05-01 11:16:47
    2|fuga||2017-05-01 11:16:50|2017-05-01 11:16:50
    3|piyo||2017-05-01 11:16:53|2017-05-01 11:16:53
    sqlite>
    ```

--- original/migrations/1_create_message_table.sql
+++ original/migrations/1_create_message_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE message (
     id INTEGER NOT NULL PRIMARY KEY,
     body TEXT NOT NULL DEFAULT "",
-    sender_name TEXT NOT NULL DEFAULT "anonymous",
+    sender_name TEXT NOT NULL DEFAULT "",
     created TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     updated TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
