0e139e21aadf2a82da6d8dbe8e18595ed5153eda
yuk-vg <yukimine_hayashi@voyagegroup.com>
2017-05-06 16:59:30 +0900

    :sparkles: utilを追加
    VG-Tech-Dojo/vg-1day-2016からコピペ

---
+++ original/bot/util.go
@@ -0,0 +1,34 @@
+package bot
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+func postJson(url string, input interface{}, output interface{}) error {
+	data, err := json.Marshal(input)
+	if err != nil {
+		return errors.Wrapf(err, "failed to decode json. data: %v", input)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return errors.Wrapf(err, "POST message: %v", data)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read response. response: %v", resp)
+	}
+
+	err = json.Unmarshal(body, &output)
+	if err != nil {
+		return errors.Wrapf(err, "failed to encode json. json: %v", &output)
+	}
+
+	return nil
+}
