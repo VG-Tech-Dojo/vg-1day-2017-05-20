fd6d46954447907c73d17c1e27c12cc02371af66
yang-wei <yangweilim21@gmail.com>
2017-05-20 23:55:12 +0900

    yang wei

---
+++ yangwei/bot/checker.go
@@ -0,0 +1,32 @@
+package bot
+
+import (
+	"regexp"
+
+	"github.com/VG-Tech-Dojo/vg-1day-2017/yangwei/model"
+)
+
+type (
+	// Checker はmessageを受け取り、条件を満たすか判定するインターフェースです
+	Checker interface {
+		Check(*model.Message) bool
+	}
+
+	// RegexpChecker は 正規表現を満たす場合true、そうでない場合falseを返す構造体です
+	RegexpChecker struct {
+		regexp *regexp.Regexp
+	}
+)
+
+// Check は正規表現を満たす場合true、そうでない場合falseを返します
+func (c *RegexpChecker) Check(m *model.Message) bool {
+	return c.regexp.MatchString(m.Body)
+}
+
+// NewRegexpChecker は新しいRegexpChecker構造体のポインタを返します
+func NewRegexpChecker(pattern string) *RegexpChecker {
+	r := regexp.MustCompile(pattern)
+	return &RegexpChecker{
+		regexp: r,
+	}
+}
---
+++ yangwei/controller/message.go
@@ -0,0 +1,105 @@
+package controller
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/VG-Tech-Dojo/vg-1day-2017/yangwei/httputil"
+	"github.com/VG-Tech-Dojo/vg-1day-2017/yangwei/model"
+	"github.com/gin-gonic/gin"
+)
+
+// Message is controller for requests to messages
+type Message struct {
+	DB     *sql.DB
+	Stream chan *model.Message
+}
+
+// All は全てのメッセージを取得してJSONで返します
+func (m *Message) All(c *gin.Context) {
+	msgs, err := model.MessagesAll(m.DB)
+	if err != nil {
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	if len(msgs) == 0 {
+		c.JSON(http.StatusOK, make([]*model.Message, 0))
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": msgs,
+		"error":  nil,
+	})
+}
+
+// GetByID はパラメーターで受け取ったidのメッセージを取得してJSONで返します
+func (m *Message) GetByID(c *gin.Context) {
+	msg, err := model.MessageByID(m.DB, c.Param("id"))
+
+	switch {
+	case err == sql.ErrNoRows:
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusNotFound, resp)
+		return
+	case err != nil:
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": msg,
+		"error":  nil,
+	})
+}
+
+// Create は新しいメッセージ保存し、作成したメッセージをJSONで返します
+func (m *Message) Create(c *gin.Context) {
+	var msg model.Message
+	if err := c.BindJSON(&msg); err != nil {
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+	if msg.Body == "" {
+		resp := httputil.NewErrorResponse(errors.New("body is missing"))
+		c.JSON(http.StatusBadRequest, resp)
+		return
+	}
+
+	// 1-2. ユーザー名を追加しよう
+	// ユーザー名が空でも投稿できるようにするかどうかは自分で考えてみよう
+
+	inserted, err := msg.Insert(m.DB)
+	if err != nil {
+		resp := httputil.NewErrorResponse(err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+
+	// bot対応
+	m.Stream <- inserted
+
+	c.JSON(http.StatusCreated, gin.H{
+		"result": inserted,
+		"error":  nil,
+	})
+}
+
+// UpdateByID は...
+func (m *Message) UpdateByID(c *gin.Context) {
+	// 1-3. メッセージを編集しよう
+	// ...
+	c.JSON(http.StatusCreated, gin.H{})
+}
+
+// DeleteByID は...
+func (m *Message) DeleteByID(c *gin.Context) {
+	// 1-4. メッセージを削除しよう
+	// ...
+	c.JSON(http.StatusOK, gin.H{})
+}
