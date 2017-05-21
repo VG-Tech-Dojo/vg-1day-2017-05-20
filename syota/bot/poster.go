a5cee4a44979d78f44bf0682c413bb1b818dc5ee
ohtukasyota <syota.jaska@gmail.com>
2017-05-21 12:01:57 +0900

    add syota.html

---
+++ syota/bot/poster.go
@@ -0,0 +1,32 @@
+package bot
+
+import (
+	"github.com/VG-Tech-Dojo/vg-1day-2017-05-20/syota/model"
+)
+
+const (
+	postURL = "http://localhost:8080/api/messages"
+)
+
+type (
+	// Poster はInに渡されたmessageをPOSTするための構造体です
+	Poster struct {
+		In chan *model.Message
+	}
+)
+
+// Run はPosterを起動します
+func (p *Poster) Run() {
+	for m := range p.In {
+		out := &model.Message{}
+		go postJSON(postURL, m, out)
+	}
+}
+
+// NewPoster は新しいPoster構造体のポインタを返します
+func NewPoster(bufferSize int) *Poster {
+	in := make(chan *model.Message, bufferSize)
+	return &Poster{
+		In: in,
+	}
+}
