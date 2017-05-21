26af3b02d9f5d6c03d3f43155026cf959fd812fa
konojunya <junya03dance@gmail.com>
2017-05-21 18:00:56 +0900

    animationの強化

--- group-4/assets/js/app.js
+++ group-4/assets/js/app.js
@@ -1,7 +1,12 @@
 // constants
 var ENTER_KEY_CODE = 13;
 var kuji_map = {
-  "daikichi": "/assets/images/kuji-daikichi.png"
+  "大吉": "/assets/images/kuji-daikichi.png",
+  "吉": "/assets/images/kuji-daikichi.png",
+  "中吉": "/assets/images/kuji-daikichi.png",
+  "小吉": "/assets/images/kuji-daikichi.png",
+  "末吉": "/assets/images/kuji-daikichi.png",
+  "凶": "/assets/images/kuji-daikichi.png",
 }
 
 // event set
@@ -40,7 +45,19 @@ function kujiRender(kuji_type){
   $(".kuji img").attr("src",image_url)
   changeView()
 
-  TweenMax.to('.kuji', 1.5, { autoAlpha: 1, ease: Expo.easeInOut });
+  kuji_type == "凶" ? doBadAnimation() : doAnimation()
+
+}
+
+function doAnimation(){
+  TweenMax.to('.kuji', 1, { autoAlpha: 1, ease: Expo.easeInOut });
+  TweenMax.to('.kuji', 1, { rotation: 360 });
+  TweenMax.to(".cracker img", 0.5, { width: "100%" })
+  TweenMax.to(".cracker img", 3, { delay: 0.5 ,autoAlpha: 0 })
+}
+
+function doBadAnimation(){
+  TweenMax.to('.kuji', 1, { autoAlpha: 1, ease: Expo.easeInOut });
 }
 
 function changeView(){
