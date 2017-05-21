ec9b1efba96926627d01d494a513be8335f729b2
takumi-n <takumi.chillout@gmail.com>
2017-05-21 15:43:02 +0900

    Close #104

--- takumi-n/bot/processor.go
+++ takumi-n/bot/processor.go
@@ -7,10 +7,12 @@ import (
 	"fmt"
 	"github.com/VG-Tech-Dojo/vg-1day-2017-05-20/takumi-n/env"
 	"github.com/VG-Tech-Dojo/vg-1day-2017-05-20/takumi-n/model"
+	"net/url"
 )
 
 const (
 	keywordApiUrlFormat = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=%s&sentence=%s&output=json"
+	talkApiUrlFormat = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk"
 )
 
 type (
@@ -30,6 +32,9 @@ type (
 
 	// GachaProcessor は"SSレア", "Sレア", "レア", "ノーマル"のいずれかをランダムで作るprocessorの構造体です
 	GachaProcessor struct{}
+
+	// メッセージ本文から会話を抽出するprocessorの構造体です
+	TalkProcessor struct{}
 )
 
 // Process は"hello, world!"というbodyがセットされたメッセージのポインタを返します
@@ -87,5 +92,38 @@ func (p *KeywordProcessor) Process(msgIn *model.Message) *model.Message {
 
 	return &model.Message{
 		Body: "キーワード：" + strings.Join(keywords, ", "),
+	}
+}
+
+// Process はメッセージ本文からキーワードを抽出します
+func (p *TalkProcessor) Process(msgIn *model.Message) *model.Message {
+	r := regexp.MustCompile("\\Atalk (.*)\\z")
+	matchedStrings := r.FindStringSubmatch(msgIn.Body)
+	text := matchedStrings[1]
+
+	api := talkApiUrlFormat
+
+	type(
+		Talk struct {
+			Perplexity float64 `json:"perplexity"`
+			Reply string `json:"reply"`
+		}
+
+		TalkResult struct {
+			Status int `json:"status"`
+			Message string `json:"message"`
+			Results []Talk `json:"results"`
+		}
+	)
+
+	json := TalkResult{}
+	param := url.Values{}
+	param.Add("apikey", env.TalkApiAppId)
+	param.Add("query", text)
+
+	post(api, param, &json)
+
+	return &model.Message{
+		Body: json.Results[0].Reply,
 	}
 }
--- takumi-n/server.go
+++ takumi-n/server.go
@@ -88,6 +88,8 @@ func (s *Server) Init(dbconf, env string) error {
 	s.bots = append(s.bots, keywordBot)
 	gachaBot := bot.NewGachaBot(s.poster.In)
 	s.bots = append(s.bots, gachaBot)
+	talkBot := bot.NewTalkBot(s.poster.In)
+	s.bots = append(s.bots, talkBot)
 
 	return nil
 }
