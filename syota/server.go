aba30a76ba56493776629c7393f64250f8e95f36
ohtukasyota <syota.jaska@gmail.com>
2017-05-21 14:56:22 +0900

    add gachaBot

--- syota/server.go
+++ syota/server.go
@@ -84,6 +84,8 @@ func (s *Server) Init(dbconf, env string) error {
 	s.bots = append(s.bots, omikujiBot)
 	keywordBot := bot.NewKeywordBot(s.poster.In)
 	s.bots = append(s.bots, keywordBot)
+	gachaBot := bot.NewGachaBot(s.poster.In)
+	s.bots = append(s.bots, gachaBot)
 
 	return nil
 }
