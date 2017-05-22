e147190decdcdfa1db61402bff20a24d2a1513de
konojunya <junya03dance@gmail.com>
2017-05-22 17:30:32 +0900

    bundle.jsをignoreに。app.jsからjqueryを削除

--- group-4/assets/js/app.js
+++ group-4/assets/js/app.js
@@ -1,16 +1,12 @@
 import Utils from "./Utils"
 
-(function($){
+let utils = new Utils()
+const ENTER_KEY_CODE = 13;
 
-  let utils = new Utils()
-  const ENTER_KEY_CODE = 13;
-
-  $(".box .user-input").keydown( e => {
-    if(e.keyCode === ENTER_KEY_CODE){
-      var keyword = $(".user-input").val()
-      $(".user-input").val("")
-      utils.requestOmikuji(keyword)
-    }
-  })
-
-})(jQuery)
+let userInput = document.querySelector(".box .user-input")
+document.addEventListener("keydown",(e) => {
+  if(e.keyCode === ENTER_KEY_CODE){
+    let keyword = userInput.value
+    utils.requestOmikuji(keyword)
+  }
+})
